e6600cf3ef23e99dc1551a2b2de16c0b2ba10021
Phil Mayer <phil.mayer93@gmail.com>
2019-10-15 18:07:20 -0400

    fix(setup): resolve some script errors

--- setup.sh
+++ setup.sh
@@ -2,39 +2,38 @@
 
 # setup.sh
 # Author: Phil Mayer
-# Modified: 9/16/19
+# Modified: 11/7/19
+
+set -euo pipefail
+IFS=$'\n\t'
+set -x
 
 # Make sure Homebrew is installed.
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# TODO: check if brew is on the PATH first
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Some of my aliases require additional programs (see the "catp" alias). Install these now.
-brew install exa jq
-
-# My dotfile configuration includes jEnv and NVM. Make sure jEnv is installed and remove any current installations of
-# NVM, as they will cause the zsh-nvm plugin to fail its setup.
-brew install jenv
+# Some of my aliases require additional programs (see the "catp" alias). I also kick off jEnv in my zsh configuration.
+# Install these dependencies now, as well as zsh (just in case, though it should be installed already).
+brew install exa jenv jq zsh
+# Remove any current installations of NVM, as they will cause the zsh-nvm plugin to fail its setup.
 rm -rf $HOME/.nvm
 
 # Copy files into place
-cp files/* ~
+/bin/cp -vr ./files/. $HOME/
 
-# Install zsh (just in case, though it should be installed already) and set as the default shell
-brew install zsh
-chsh -s /bin/zsh
-
-# Remove bash configuration files and move overrides files
+# Remove bash configuration files and move overrides files.
 rm ~/.bash_profile ~/.bash_aliases
 mv ~/.bash_overrides ~/.zsh_overrides
 
-# Install oh-my-zsh
+# Install oh-my-zsh.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install powerline fonts
+# Install powerline fonts.
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
 cd ..
 rm -rf fonts
 
-# Install oh-my-zsh plugins
+# Install oh-my-zsh plugins.
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
